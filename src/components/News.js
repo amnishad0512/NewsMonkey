446fb458846b902067ed3b59efd4fc6b33195650
Manoj <amnishad0512@gmail.com>
2022-05-04 15:29:55 +0530

    infinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import { NewsItem } from "./NewsItem";
 import { Spinner } from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
   static defaultProps = {
     q: "nokia",
+    totalResults:0
+
   };
   static propTypes = {
     q: PropTypes.string,
@@ -16,6 +19,7 @@ export class News extends Component {
       articles: [],
       loading: false,
       page: 1,
+      totalResults:0
     };
   }
   async componentDidMount() {
@@ -25,7 +29,11 @@ export class News extends Component {
     );
 
     data = await data.json();
-    this.setState({ articles: data.articles, loading: false });
+    this.setState({
+      articles: data.articles,
+      loading: false,
+      totalResults: data.totalResults,
+    });
   }
 
   previous = async () => {
@@ -43,6 +51,7 @@ export class News extends Component {
       articles: data.articles,
       page: this.state.page - 1,
       loading: false,
+      totalResults: data.totalResults,
     });
   };
   next = async () => {
@@ -59,23 +68,44 @@ export class News extends Component {
       articles: data.articles,
       page: this.state.page + 1,
       loading: false,
+      totalResults: data.totalResults,
     });
   };
 
-  capatialize(string){
-   let text = string.toLowerCase();
-   let firstChar = text.charAt(0).toUpperCase();
-   return firstChar+text.slice(1);
+  capatialize(string) {
+    let text = string.toLowerCase();
+    let firstChar = text.charAt(0).toUpperCase();
+    return firstChar + text.slice(1);
   }
+   fetchMoreData = async() => {
+    this.setState({page:this.state.page+1})
+    let data = await fetch(
+      `https://newsapi.org/v2/everything?q=${this.props.q}&apiKey=07d673cfec874afda8fb694c94d67ae6&page=${this.state.page}&pageSize=21`
+    );
+
+    data = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(data.articles),
+      loading: false,
+      totalResults: data.totalResults,
+    });
+  };
 
   render() {
     return (
       <div className="container">
-        <h5 className="my-3 text-center">NewsMonkey - {`${this.capatialize(this.props.q)}`}</h5>
-        {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((newsItem) => {
+        <h5 className="my-3 text-center">
+          NewsMonkey - {`${this.capatialize(this.props.q)}`}
+        </h5>
+        {/* {this.state.loading && <Spinner />} */}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="row">
+            {this.state.articles.map((newsItem) => {
               return (
                 <div className="col-lg-4 my-3">
                   {" "}
@@ -90,8 +120,9 @@ export class News extends Component {
                 </div>
               );
             })}
-        </div>
-        <div className="container d-flex justify-content-between my-3">
+          </div>
+        </InfiniteScroll>
+        {/* <div className="container d-flex justify-content-between my-3">
           <button
             className="btn btn-dark"
             disabled={this.state.page <= 1}
@@ -107,7 +138,7 @@ export class News extends Component {
           >
             Next &#8594;
           </button>
-        </div>
+        </div> */}
       </div>
     );
   }
